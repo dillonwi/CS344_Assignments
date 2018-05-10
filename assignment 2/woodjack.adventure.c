6b295e264216cc7f59cb754f4ee816775b829f2d
Jack Woods <jackrwoods@gmail.com>
2018-05-09 22:16:57 -0700

    Backup

--- assignment 2/woodjack.adventure.c
+++ assignment 2/woodjack.adventure.c
@@ -28,23 +28,115 @@ struct Room {
  char* roomType;  /* stores the index of neighboring rooms. */
 };
 
+/* Create array of room pointers */
+Room* rooms[7];
+
+Room* loadRooms() {
+
+  DIR *d;
+  struct dirent *de;
+  d = opendir(".");
+  if (d) {
+    int found = 0;
+    while ((de = readdir(d)) != NULL) {
+
+      char buffer[15];
+      snprintf(buffer, 15, "%s", de->d_name);
+      printf("%s\n", buffer);
+
+      if (strcmp(buffer, "woodjack.rooms.") != 0) {
+        found = 1;
+        printf("yeah");
+        break; /* Found it! */
+      }
+
+    }
+    closedir(d);
+  }
+
+  // /* Look for directory with rooms */
+  // struct dirent* de;  /* Pointer for directory entry */
+  // DIR* dr = opendir(".");
+  // assert (dr != NULL);
+  // int found = 0;
+  // while (de = readdir(dr)) {
+  //   char buffer[15];
+  //   snprintf(buffer, 15, "%s", de->d_name);
+  //   printf("%s\n", buffer);
+  //   if (strcmp(buffer, "woodjack.rooms.") != 0) {
+  //     found = 1;
+  //     printf("yeah");
+  //     break; /* Found it! */
+  //   }
+  // }
+  // closedir(dr);
+
+  // /* Open the directory that was found */
+  // DIR* dr2 = opendir(de->d_name);
+  // closedir(dr);
+  //
+  // /* Load the contents of each file into a room array */
+  // while ((de = readdir(dr2)) != NULL) {
+  //   printf("Read dir: %s\n", de->d_name);
+  //   //readContents(de->d_name);
+  // }
+  //
+  // /* Search for the starting room */
+  // int i;
+  // while (i < 7) {
+  //   if (rooms[i]->roomType == "START_ROOM") {
+  //     /* Return a pointer to the starting room */
+  //     return rooms[i];
+  //   }
+  // }
+  // return rooms[0];
+}
+
+void printLocation(Room* r) {
+  char buffer[100];
+  sprintf(buffer, "Current Location: %s", r->name);
+}
 
 int main() {
   /* Load rooms into memory */
+  Room* r = loadRooms();
 
   /* Start game loop */
+  while (1) {
     /* If this isn't the end room... */
-      /* Print the name of the room */
-      /* Print possible connections */
-      /* Ask "WHERE TO? >" */
-      /* Parse user input */
-      /* If invalid, print "HUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN." */
-      /* Save user's choice (if valid) to path history and step count. */
-    /* If this is the end room... */
-      /* Print the name of the room, and indicate this is the end room. */
-      /* Print the number of steps, and the path */
-      /* Print "Congratulations!" */
-      printf("Congratulations!\n")
-      /* Exit the application */
-      return 0;
+    if (r->roomType != "END_ROOM") {
+        /* Print the name of the room */
+        //printLocation(r);
+
+        /* Print possible connections */
+        //char** connections = printConnections(r);
+
+        /* Ask "WHERE TO? >" */
+        printf("WHERE TO? >");
+
+        /* Parse user input */
+
+        /* If invalid, print "HUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN." */
+        /* Save user's choice (if valid) to path history and step count. */
+    } else {
+      /* If this is the end room... */
+        /* Print the name of the room, and indicate this is the end room. */
+        printLocation(r);
+        printf("This is the end room.\n");
+
+        /* Print the number of steps, and the path */
+        /* Print "Congratulations!" */
+        printf("Congratulations!\n");
+        /* Exit the application */
+        break;
+    }
+  }
+
+  /* Free memory associated with rooms */
+  int i;
+  for (i = 0; i < 7; i++) {
+    free(rooms[i]);
+  }
+
+  return 0;
 }

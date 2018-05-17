72ee4306be7fc1eda92fdcc3c8d409578fe147f9
Jack Woods <jackrwoods@gmail.com>
2018-05-16 17:12:30 -0700

    Add random characters and file creation.

---
+++ assignment 3/mypython.py
@@ -0,0 +1,51 @@
+import random
+import os
+
+# Create a file, assign it a unique name, and return the file
+def createFile(numFiles, processID):
+    name = str(numFiles) + "-" + str(processID)
+    return open(name, "w+")
+
+def randomChar():
+    num = random.randint(0,26) + 97
+    return str(chr(num))
+
+
+# Create n files and populate them with 10 random characters in the lowercase
+# alphabet.
+def randomChars(n):
+    numFiles = 0
+    processID =  os.getpid()
+    # Iterate n times
+    for i in range(n):
+
+        # Call createFile()
+        file = createFile(numFiles, processID)
+        numFiles = numFiles + 1
+
+        # Iterate and call randomChar() 10 times. Print each character to stdout
+        # and populate the file
+        for j in range(10):
+            c = randomChar()
+            print(c, end='')
+            file.write(c)
+        print() # New line
+
+        # Add an endline character at the end of the file, and print a newline char
+        # to stdout
+        file.write("\n")
+
+        # Close the file
+        file.close()
+
+# Calculate n random numbers from 1 to 42 inclusive, print the numbers
+# to stdout, and then multiply them.
+def randomNums(n):
+    print(1)
+
+# Main function
+def main():
+    randomChars(3)
+    randomNums(2)
+
+main()
